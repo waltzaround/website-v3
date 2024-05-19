fd456e160c5d151c557aaa39b3a16189cfa41fc8
Walter Lim <waltissomewhere@gmail.com>
2024-05-19 23:41:21 +1200

    fix a11y

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,24 +1,24 @@
 const ProjectCard = ({ project }: { project: any }) => {
-    return (
-      <a className="project-card" href={project.url} target="_blank">
-        <img src={project.image} alt={project.title} />
-        {project.development && (
-          <div className="project-development">Under active development</div>
-        )}
-        <div className="project-card-content">
-          <h3 className="project-card-title">{project.name}</h3>
-          <p>{project.description}</p>
-          <aside className="project-date">{project.date}</aside>
+  return (
+    <a className="project-card" href={project.url} target="_blank">
+      <img src={project.image} alt={project.name} />
+      {project.development && (
+        <div className="project-development">Under active development</div>
+      )}
+      <div className="project-card-content">
+        <h3 className="project-card-title">{project.name}</h3>
+        <p>{project.description}</p>
+        <aside className="project-date">{project.date}</aside>
+      </div>
+      <div className="project-tags">
+        <div className="project-work-type">
+          {project.categories.map((categories: any) => (
+            <p className="project-work-type-tag">{categories}</p>
+          ))}
         </div>
-        <div className="project-tags">
-          <div className="project-work-type">
-            {project.categories.map((categories: any) => (
-              <p className="project-work-type-tag">{categories}</p>
-            ))}
-          </div>
-        </div>
-      </a>
-    );
-  };
+      </div>
+    </a>
+  );
+};
 
-  export default ProjectCard;
+export default ProjectCard;
