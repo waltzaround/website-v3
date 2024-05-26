32f3fb96551aeaf91efa1e7be928a8350df1d38e
Walter Lim <waltissomewhere@gmail.com>
2024-05-26 17:10:11 +1200

    content

--- src/pages/case-studies/Cin7.tsx
+++ src/pages/case-studies/Cin7.tsx
@@ -9,9 +9,9 @@ function Cin7() {
         <h1 className="story-heading">
           <strong>Cin7.com</strong>
           <br />
-          Helping 8,000+ businesses transact billions of dollars
-          by helping them track and sell (legal) guns,
-          (legal) drugs, alcohol, peanut butter, and sex toys across the world, among other products.
+          Helping 8,000+ businesses transact billions of dollars by helping them
+          track and sell (legal) guns, (legal) drugs, alcohol, peanut butter,
+          and sex toys across the world, among other products.
         </h1>
         <img src="/images/projects/cin7.webp" className="story-hero" />
 
@@ -23,7 +23,8 @@ function Cin7() {
                 Cin7 is an inventory management system that helps people that
                 sell physical products track stock, sales, and customers across
                 every imaginable online and offline retail channel.
-              </p><br/>
+              </p>
+              <br />
             </div>
             <div>
               <h2>Role</h2>
@@ -58,8 +59,8 @@ function Cin7() {
               </li>
 
               <li>
-                Successfully condensed 10 different fragmented design systems into a single unified
-                system
+                Successfully condensed 10 different fragmented design systems
+                into a single unified system
               </li>
               <li>
                 Reduced churn by millions of dollars by implementing usability
@@ -76,99 +77,231 @@ function Cin7() {
         <section className="">
           <h2 className="main-heading">How I got involved</h2>
           <p className="main-text">
-            I joined Cin7 as a Senior Product Designer in 2019 after being
+            I joined Cin7 as a Senior Product Designer in late 2020 after being
             shoulder tapped by an ex-colleague who was running the company at
             the time.
+          </p>
+          <p className="main-text">
+            I did my homework before I joined, and I learned that the company
+            was effectively a mini-ERP, that helped small businesses sell stuff
+            everywhere simultaneously from retail outlets, to shopify stores and
+            big box retailers like walmart. The company was an order of
+            magnitude cheaper than traditional ERPs, and was one of the easiest
+            ways that customers could set up EDI connections to big retailers
+            that required it as part of the sales process.
           </p>
           <p className="main-text">
             I was the first designer to be hired in a long time, and the company
             was in a state of turbulent flux. The company was growing rapidly,
             and the product was growing erratically with it. I had a joined a
             company with significant technical debt, and a product that was
-            starting to show immense levels of system fragmentation.     </p>
-          <p className="main-text">Think about
-            it this way - product design at the time was owned by an dev
-            team of 50 people. Zero designers were involved in the product or engineering process. 
+            starting to show immense levels of system fragmentation.{" "}
           </p>
-      
-         
-       
+          <p className="main-text">
+            Think about it this way - product design at the time was owned by an
+            dev team of 50 people. Zero designers were involved in the product
+            or engineering process.
+          </p>
           <p className="main-text">
             It resulted in a product platform that felt like it was held
-            together by duct tape across the product platform. 
+            together by duct tape across the product platform.
           </p>
-          <img src="/images/cin7/sitemap-2020.png" className="story-hero" />
+          <div className="main-image-container">
+            <img
+              src="/images/cin7/sitemap-2020.png"
+              className="main-image-context"
+            />
+            <p className="main-image-context-text">
+              A sitemap I put together when I first started that gives you a
+              rough feel for the size of the system - this doesn't include the
+              hundreds of integrations and other system configuration pages like
+              Amazon Marketplace, or any of our internal tooling.
+            </p>
+          </div>
+          <section className="">
+            <h2 className="main-heading">Initial discovery</h2>
 
-        
-
-        <section className="">
-        <h2 className="main-heading">First impressions</h2>
-
-        <p className="main-text">
-       Day one was a little wierd. I was assigned an absurdly overpowered developer laptop, and just told to go fix things. Instead, I decided to try and do some design discovery: </p>
-        <p className="main-text">
-            I spent a lot of time just listening to staff and customers when I started.   </p>
             <p className="main-text">
-           Watching how the team worked. Observing what internal and external processes looked like.   </p>
-          
+              Day one was a little wierd. Given that the company didn't know
+              what to do with a designer, I was assigned an absurdly overpowered
+              developer laptop with an hour of battery life, and just told to go
+              fix things. Instead, I decided to try and do some design
+              discovery:{" "}
+            </p>
+            <p className="main-text">
+              - I spent a lot of time just listening to staff and customers.{" "}
+            </p>
+            <p className="main-text">
+              - Observing how the team worked. Learning about what internal and
+              external processes looked like.{" "}
+            </p>
 
-        <p className="main-text">
-      How they triaged critical bugs and angry customers when the system shat itself.</p>
-      <p className="main-text">
-      How they prioritised new initiatives against existing fires.</p>
-      <p className="main-text">
-      
-    Seemed like almost all the engineering capacity was devoted to bugs and maintaining existing integrations with zero time for polish. To be fair, I think it was the right call - certain bugs meant that customers lost money, and in some cases, massive amounts.</p>
+            <p className="main-text">
+              - How they triaged critical bugs and angry customers when the
+              system shat itself.
+            </p>
+            <p className="main-text">
+              - How they prioritised new initiatives against existing fires.
+            </p>
+            <p className="main-text">
+              Seemed like almost all the engineering capacity was devoted to
+              bugs and maintaining existing integrations with zero time for
+              polish. To be fair, I think it was the right call - certain bugs
+              meant that customers lost money, and in some cases, massive
+              amounts.
+            </p>
 
-    <p className="main-text">
-    But at the same time, I rapidly discovered that I was in a low design maturity organisation. For those that don't understand the concept of design maturity - it means that design as a discipline isnt really leveraged in the product development process to get better results.</p>
+            <p className="main-text">
+              But at the same time, I rapidly discovered that I was in a low
+              design maturity organisation. For those that don't understand the
+              concept of design maturity - it means that design as a discipline
+              isnt really leveraged in the product development process to get
+              better results.
+            </p>
           </section>
-          <img src="/images/cin7/pos.png" className="story-hero" />
+          <div className="main-image-container">
+            <img src="/images/cin7/pos.png" className="main-image-context" />
+            <p className="main-image-context-text">
+              A screenshot of our point of sale webapp in 2020 - product design
+              was kind of an afterthought at the time and it showed. We would
+              even advertise upsells to customers that were already on the
+              highest tier of our product. This advertising was one of the first
+              things I got rid of upon getting access to the codebase.
+            </p>
+          </div>
+          <h2 className="main-heading">Getting crushed by tech debt</h2>
+          <p className="main-text">
+            I jumped into the work headfirst, and supported my team on every
+            feature and bugfix I could get my hands on. and rapidly discovered a
+            rats nest of problems.
+          </p>
+          <p className="main-text">
+            {" "}
+            The code was 20+ years old. I'm talking multiple different versions
+            of react, angular, and ASP.Net codebases smoooshed into a single
+            app. The top navigation component was a separate react app to the
+            current page. Likewise with the sidebar navigation. All of this was
+            wrapped in an ASP.Net application running on an ancient .NET
+            Framework backend.
+          </p>{" "}
+          <div className="main-image-container">
+            <img src="/images/cin7/users.png" className="main-image-context" />
+            <p className="main-image-context-text">
+              Notice the fragmentation yet? Depending on the developer and
+              product manager working on it primary buttons within the app could
+              be any of 50 different implementations.
+            </p>
+          </div>
+          <p className="main-text">
+            The application was difficult to design for; a quick look in both
+            the codebase and browser inspector told me that almost every
+            component had 50 different visually-different implementations across
+            the hundreds of pages on the platform. Thats before you get into the
+            fact that every component and page was written as nested{" "}
+            <code>{`<table>`}</code> elements 20 tables deep with hardcoded
+            explicit dimensions. Even the buttons you see below were nested{" "}
+            <code>{`<table>`}</code> elements.
+          </p>
+          <p className="main-text">
+            This was exacerbated by the fact that we didnt have any designers
+            before I joined; every product manager was responsible for design on
+            each of the products/module they looked after - this resulted in
+            immense experience fragmentation both in user flows, component
+            libraries and product norms. This meant that every feature
+            effectively felt like a different product you had to relearn how to
+            use every time you changed page.
+          </p>
+          <p className="main-text">
+            It was horrific for usability and onboarding and the numbers showed
+            it.
+          </p>
+          <img src="/images/cin7/CRM.png" className="main-image" />
+          <img src="/images/cin7/appstore.png" className="main-image" />
+          <h2 className="main-heading">
+            Responding with a technical frontline approach
+          </h2>
+          <p className="main-text">
+            In response to this, the first thing I proposed was a consolidation;
+            impossible to tackle piecemeal - four designers before me had tried
+            to do it module by module and failed miserably resulting in ven more
+            experience fragmentation. I proposed a complete overhaul of the
+            design system, and a partial re-architecture of the front end to
+            simplify the experience.
+          </p>
+          <p className="main-text">
+            To get stakeholder buy in across product, management, and
+            engineering I put together a high level sitemap of the system with
+            figma which you can see above, and then mapped out the current state
+            of the platform.
+          </p>
+          <p className="main-text">
+            I then did some rapid prototyping in our dev environment over a day
+            and a half by using CSS editor chrome extensions. I would've used
+            Figma, but the fact that this product was so data entry heavy meant
+            that I had to build a lot of the interactions in code to get a feel
+            for how the system would work. It also meant that I could do rapid
+            usability testing without spending too much time building out the
+            interactions in figma.
+          </p>
+          <div className="main-image-container">
+            <img
+              src="/images/cin7/css-editor.png"
+              className="main-image-context"
+            />
+            <p className="main-image-context-text">
+              An example of how I rapidly prototyped using CSS editor extensions
+              - a lot of this hinged on knowledge of CSS specificity and how the
+              box model behaved.
+            </p>
+          </div>
+          <p className="main-text">
+            {" "}
+            This approach quickly and cheaply proved my hypothesis that we could
+            make easy wins with css, at the cost of minimal additional tech
+            debt. It also meant that we could cycle through iterations in
+            minutes, instead of spending weeks building out text input
+            prototypes in figma that were incapable of emulating large amounts
+            of data entry.
+          </p>
+          <p className="main-text">
+            The prototype was a hit with management, and I was given the go
+            ahead to start working on the design system on the codebase as a
+            product designer, which was hosted on Azure Devops at the time.
+          </p>
+          <p className="main-text">
+            A lot of the work after this included compromising with engineering
+            to get the design system into place without paying off the technical
+            debt
+          </p>
+        </section>
 
-        <h2 className="main-heading">Initial Discovery</h2>
-        <p className="main-text">
-       I jumped into the work headfirst, and supported my team on every feature and bugfix I could get my hands on. and rapidly discovered a rats nest of problems.</p>
-       <p className="main-text"> The code was 20+ years old. I'm talking multiple different versions of react, angular, and ASP.Net codebases smoooshed into a single app. The top navigation component was a separate react app to the current page. Likewise with the sidebar navigation. All of this was wrapped in an ASP.Net application running on an ancient .NET Framework backend.</p>    <img src="/images/cin7/users.png" className="story-hero" />
+        <section>
+          <h2 className="main-heading">Sprinting designs to production</h2>
+          <p className="main-text">It worked.</p>
 
-       <p className="main-text">
-       The application was difficult to design for; a quick look in both the codebase and  browser inspector told me that almost every component had 50 different visually-different implementations across the hundreds of pages on the platform. Thats before you get into the fact that every component and page was written as nested <code>{`<table>`}</code> elements 20 tables deep with hardcoded explicit dimensions. Even the buttons were <code>{`<table>`}</code> elements.
-       </p>
-       <img src="/images/cin7/CRM.png" className="story-hero" />
-        
+          <img src="/images/cin7/css.png" className="main-image" />
+        </section>
+        <section>
+          <h2 className="main-heading">Solidyfing the system</h2>
+          <p>Reducing time to market and increasing development velocity</p>
 
-      <p className="main-text">
-        
-       
-       
-       This was exacerbated by the fact that we didnt have any designers before I joined; every product manager was responsible for design on each of the products/module they looked after - this resulted in immense design fragmentation both in user flows, component libraries and product norms. </p>
-       <img src="/images/cin7/appstore.png" className="story-hero" />
-       <h3>Initiative 1: Simplify the experience</h3>
-       <p className="main-text">
-        In response to this, the first thing I proposed was a consolidation;
+          <p>Making it easier to develop and maintain the platform</p>
 
-        impossible to tackle piecemeal - four designers before me had tried to do it module by module and failed miserably. I proposed a complete overhaul of the design system, and a partial re-architecture of the front end to simplify the experience.
-       </p>
-       <p className="main-text">
-        To get stakeholder buy in across product, management, and engineering 
-        
-       I put together a high level sitemap of the system with figma, and mapped out the current state of of the platform.
-       </p>
-       <p className="main-text">
-        I then did some rapid prototyping in our dev environment by using CSS chrome extensions. 
-       
-        I would've used figma, but the fact that this product was so data entry heavy meant that I had to build a lot of the interactions in code to get a feel for how the system would work.
-        
-
-       </p>
-       <img src="/images/cin7/css.png" className="story-hero" />
-       
-       <p className="main-text">
-        compromising with engineering to get the design system into place without paying off the technical debt
-
-       </p>
-       
-</section><section>
-{/* 
+          <div className="main-image-container">
+            <img src="/images/cin7/ds3.png" className="main-image-context" />
+            <p className="main-image-context-text">Aaaa subtitle</p>
+          </div>
+          <div className="main-image-container">
+            <img src="/images/cin7/ds1.png" className="main-image-context" />
+            <p className="main-image-context-text">Aaaa subtitle</p>
+          </div>
+          <div className="main-image-container">
+            <img src="/images/cin7/ds2.png" className="main-image-context" />
+            <p className="main-image-context-text">Aaaa subtitle</p>
+          </div>
+        </section>
+        <section>
+          {/* 
           <section>  <p className="main-text">
            I can probably break down my impact at Cin7 into
             four areas:
@@ -181,7 +314,7 @@ function Cin7() {
             <p>Product experience</p>
           </div>
         </section> */}
-</section>
+        </section>
       </section>
       <Footer />
     </>
