a0c87448f8a021ceaf86b7d6eaa4ad69f2517fd9
Walter Lim <waltissomewhere@gmail.com>
2024-05-29 00:33:39 +1200

    seo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,22 +7,42 @@ function Footer() {
       <div className="footer-cols">
         <div className="footer-cols-items">
           <h4>Social:</h4>
-          <a>Discord</a>
-          <a>LinkedIn</a>
-          <a>Twitter</a>
-          <a>Instagram</a>
+          <a href="http://discordapp.com/users/waltzaround" title="discord">
+            Discord
+          </a>
+          <a href="https://www.linkedin.com/in/waltzaround/" title="linkedin">
+            Linkedin
+          </a>
+          <a href="https://twitter.com/iwaltzaround" title="twitter">
+            Twitter
+          </a>
         </div>
         <div className="footer-cols-items">
           <h4>Find more work on</h4>
-          <a>Github</a>
-          <a>YouTube</a>
-          <a>itch.io</a>
-          <a>Behance</a>
-          <a>Dribbble</a>
+          <a href="https://github.com/waltzaround" title="github">
+            Github
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCbSAF9R8VhlT29nh3-6mqmA"
+            title="youtube"
+          >
+            YouTube
+          </a>
+          <a href="https://waltzaround.itch.io/" title="itch.io">
+            itch.io
+          </a>
+          <a href="https://behance.net/waltzaround" title="behance">
+            Behance
+          </a>
+          <a href="https://dribbble.com/waltzaround" title="dribbble">
+            Dribbble
+          </a>
         </div>
       </div>
 
-      <p className="footer-end">© {currentYear} Walter Lim - All rights reserved </p>
+      <p className="footer-end">
+        © {currentYear} Walter Lim - All rights reserved{" "}
+      </p>
     </footer>
   );
 }
