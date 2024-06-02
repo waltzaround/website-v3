9e553891c503d55055f3b230330d69328e7e80ee
Walter Lim <waltissomewhere@gmail.com>
2024-06-02 23:59:04 +1200

    sitemap update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Sitemap()
-
-  ],
+  plugins: [react()],
 })
